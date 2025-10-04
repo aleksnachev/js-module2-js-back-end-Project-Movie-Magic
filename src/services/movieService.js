879a343e8c50de030f8284fa857ee780e43fd8b6
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-10-04 20:04:40 +0300

    add getOne service method

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -4,11 +4,13 @@ export default{
     getAll() {
         return Movie.find()
     },
+    getOne(movieId){
+        return Movie.find({_id: movieId})
+    },
     create(movieData){
   
         const movie = new Movie(movieData)
         return movie.save()
 
-        
     }
 }
