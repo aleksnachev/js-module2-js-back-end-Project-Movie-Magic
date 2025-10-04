7fa5a3de9bfe626648250d43d8b38a1b09fe9115
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-10-04 20:15:05 +0300

    add findOne to movie model

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -19,6 +19,7 @@ movieController.post('/create', (req,res) => {
 movieController.get('/:movieId/details', (req,res) => {
     const movieId = req.params.movieId
     const movie = movieService.getOne(movieId)
+    
     console.log(movie)
     
     res.end()
