9c7019d81d053a2660cdfb956bdb4b9c7f2fa8bf
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-10-04 19:21:59 +0300

    Movie service create

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
+import movieService from "../services/movieService.js";
 
 const movieController = Router()
 
 movieController.get('/create', (req,res) => {
-
     res.render('create')
 })
 
 movieController.post('/create', (req,res) => {
-    console.log(req.body);
+    const movieData = req.body
+    movieService.create(movieData)
+
     res.end()
     
 })
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,5 +3,10 @@ import Movie from "../models/Movie.js";
 export default{
     getAll() {
         return Movie.find()
+    },
+    create(movieData){
+        //TODO Create movie
+        console.log(movieData);
+        
     }
 }
